3fc3086a59e8d63b8e16eac2c3cd95340081b163
JakeBertello <jake.bertello@gmail.com>
2021-11-10 18:03:12 -0600

    starting to implement position tracking

--- include/positiontracker/positiontracker.h
+++ include/positiontracker/positiontracker.h
@@ -3,4 +3,57 @@
 
 #include "main/main.h"
 
+namespace position_tracker {
+    class PositionTracker {
+        float leftEncWheelD = DEFAULT_ENC_WHEEL_D;
+        float rightEncWheelD = DEFAULT_ENC_WHEEL_D;
+        float strafeEncWheelD = DEFAULT_ENC_WHEEL_D;
+
+        float currLeftEnc = 0;
+        float currRightEnc = 0;
+        float currStrafeEnc = 0;
+
+        float prevLeftEnc = 0;
+        float prevRightEnc = 0;
+        float prevStrafeEnc = 0;
+
+        float globLeftEnc = 0;
+        float globRightEnc = 0;
+        float globStrafeEnc = 0;
+        
+        float deltaL = 0;
+        float deltaR = 0;
+        float deltaS = 0;
+
+        float deltaA = 0;
+        float deltaX = 0;
+        float deltaY = 0;
+
+        float currX = 0;
+        float currY = 0;
+        float currA = 0;
+
+        float distTraveled = 0;
+        float distTraveledStrafe = 0;
+
+        // bool positionTrackerFirstRun = true;
+
+    public:
+        void reset();
+
+        float getCurrLeftEnc();
+        float getCurrRightEnc();
+        float getCurrStrafeEnc();
+
+        float getCurrX();
+        float getCurrY();
+        float getCurrA();
+
+
+    private: 
+        float distWheelMoved(float ticks, float wheelD, float ticksPerRotation);
+
+    };
+}
+
 #endif  // WISCOPROS2122_POSITIONTRACKER_POSITIONTRACKER_H_
--- include/positiontracker/positiontrackercontroller.h
+++ include/positiontracker/positiontrackercontroller.h
@@ -2,5 +2,13 @@
 #define WISCOPROS2122_POSITIONTRACKER_POSITIONTRACKERCONTROLLER_H_
 
 #include "main/main.h"
+#include "drive/drive.h"
+
+namespace position_tracker_controller {
+    class PositionTrackerController {
+     private:
+        drive::Drive drive;
+    };
+}
 
 #endif  // WISCOPROS2122_POSITIONTRACKER_POSITIONTRACKERCONTROLLER_H_
