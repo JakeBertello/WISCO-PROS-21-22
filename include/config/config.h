fece370a29b300b3edca01bc2ddb94a8ce4f1df1
Ender717 <73455333+Ender717@users.noreply.github.com>
2021-11-30 19:39:10 -0600

    Added javadocs to config.h and part of driveconfig.h

--- include/config/config.h
+++ include/config/config.h
@@ -1,3 +1,14 @@
+/**
+ * @file config.h
+ * @author Jacob Bertello
+ * @brief This file contains the configuration for the controller
+ * @version 1.0
+ * @date 2021-11-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 // Inclusion guard
 #ifndef WISCOPROS2122_CONFIG_CONFIG_H_
 #define WISCOPROS2122_CONFIG_CONFIG_H_
@@ -7,10 +18,23 @@
 #include "misc/controllerinputs.h"
 #include "misc/inputconverter.h"
 
-// Defines a namespace for the listed items
-namespace config {
+/**
+ * @brief This namespace contains the configuration items/methods for the system.
+ * 
+ */
+namespace config 
+{
+    /**
+     * @brief The controller which allows the user to control the system
+     * 
+     */
     extern pros::Controller master;
 
+    /**
+     * @brief Set which button runs the autonomous routine
+     * 
+     * @param btn The button being set to run the auton
+     */
     extern void setAutonRunBtn(controller_input btn);
 }
 
--- include/config/driveconfig.h
+++ include/config/driveconfig.h
@@ -1,3 +1,14 @@
+/**
+ * @file driveconfig.h
+ * @author Jacob Bertello
+ * @brief This file sets up the configuration for the drivetrain of the robot
+ * @version 1.0
+ * @date 2021-11-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 // Inclusion guard
 #ifndef WISCOPROS2122_CONFIG_DRIVECONFIG_H_
 #define WISCOPROS2122_CONFIG_DRIVECONFIG_H_
@@ -11,33 +22,124 @@
 #include "config/config.h"
 #include "main/main.h"
 
-// Defines a namespace for the listed items
+/**
+ * @brief Defines the namespace which contains the objects used to initialize the drive 
+ * 
+ */
 namespace drive_config {
 
     /********************************* DRIVE_MOTORS *********************************/
+    
+    /**
+     * @brief The front left motor on the drivetrain
+     * 
+     */
     extern pros::Motor frontLeftDrive;
+
+    /**
+     * @brief The middle left motor on the drivetrain
+     * 
+     */
     extern pros::Motor middleLeftDrive;
+
+    /**
+     * @brief The rear left motor on the drivetrain
+     * 
+     */
     extern pros::Motor backLeftDrive;
+
+    /**
+     * @brief The front right motor on the drivetrain
+     * 
+     */
     extern pros::Motor frontRightDrive;
+
+    /**
+     * @brief The middle right motor on the drivetrain
+     * 
+     */
     extern pros::Motor middleRightDrive;
+
+    /**
+     * @brief The rear right motor on the drivetrain
+     * 
+     */
     extern pros::Motor backRightDrive;
 
     /********************************* DRIVE_SENSORS *********************************/
+
+    /**
+     * @brief The rotation sensor on the left tracking wheel
+     * 
+     */
     extern pros::Rotation leftRot;
+
+    /**
+     * @brief The rotation sensor on the right tracking wheel
+     * 
+     */
     extern pros::Rotation rightRot;
+
+    /**
+     * @brief The rotation sensor on the strafe tracking wheel
+     * 
+     */
     extern pros::Rotation strafeRot;
+
+    /**
+     * @brief The inertial sensor
+     * 
+     */
     extern pros::Imu imu;
+
+    /**
+     * @brief The distance sensor on the left side of the robot
+     * 
+     */
     extern pros::Distance leftDistance;
+
+    /**
+     * @brief The distance sensor on the right side of the robot
+     * 
+     */
     extern pros::Distance rightDistance;
 
     /********************************* DRIVE_CONFIG *********************************/
+
+    /**
+     * @brief The object which defines the drivetrain
+     * 
+     */
     extern drive::Drive drive;
+
+    /**
+     * @brief The object in charge of managing the operator input drive control
+     * 
+     */
     extern op_drive_controller::OpDriveController driveController;
+
+    /**
+     * @brief The object which tracks the current position of the drive
+     * 
+     */
     extern position_tracker::PositionTracker positionTracker;
+
+    /**
+     * @brief The object which controls the position tracking system
+     * 
+     */
     extern position_tracker_controller::PositionTrackerController positionTrackerController;
 
+    /**
+     * @brief The object in charge of managing the autonomous drive control
+     * 
+     */
     extern auton_drive_controller::AutonDriveController autonDriveController;
 
+    /**
+     * @brief The task which updates the position tracker
+     * 
+     */
     extern pros::Task positionTrackerTask;
 
     /********************************** DRIVE_PID ***********************************/
